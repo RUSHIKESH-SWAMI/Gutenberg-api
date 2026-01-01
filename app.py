5e26109d41acc08799af26259fb3218a7a780c2a
Rushikesh Swami <rushiswami1906@gmail.com>
2026-01-01 19:50:22 +0530

    fix flask host and port

--- app.py
+++ app.py
@@ -72,4 +72,7 @@ def books():
     return jsonify(list(books.values()))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
